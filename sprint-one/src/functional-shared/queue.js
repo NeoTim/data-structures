206f9c140b7fcbc05235c5e2948409b066c1663c
hisimagination <joel.design@icloud.com>
2014-06-05 12:14:56 -0700

    completed functional-shard and prototypal queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,8 @@ var makeQueue = function(){
   // but try not not reference your old code in writing the new style.
   var instance = {};
 	instance._storage = {};
-  instance._size = 0;
+  instance._front = 0;
+  instance._back = 0;
 
   _(instance).extend(queueMethods);
   return instance;
@@ -11,22 +12,20 @@ var makeQueue = function(){
 
 var queueMethods = {
 	enqueue: function( value ){
-		this._storage[this._size] = value;
-		this._size ++;
+		this._storage[this._back] = value;
+		this._back ++;
 	},
 	dequeue: function( ){
 		var result; 
-		if(this._size > 0){
-			this._size--;
-			result = this._storage[this._size];
-			delete this._storage[this._size];
+		if(this._back - this._front){
+			result = this._storage[this._front];
+			delete this._storage[this._front];
+			this._front ++;
+			return result;
 		}
-		return result;
+		return 0;
 	},
 	size: function( ){
-		return this._size;
+		return this._back - this._front;
 	}
 };
-
-
-
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance._storage = {};
+  instance._front = 0;
+  instance._back = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
 
+var queueMethods = {}
+
+queueMethods.enqueue = function( value ){
+	this._storage[this._back] = value;
+	this._back ++;
+};
+
+queueMethods.dequeue = function( ){
+	var result; 
+	if(this._back - this._front){
+		result = this._storage[this._front];
+		delete this._storage[this._front];
+		this._front ++;
+		return result;
+	}
+	return 0;
+};
+
+queueMethods.size = function( ){
+	return this._back - this._front;
+};
 
