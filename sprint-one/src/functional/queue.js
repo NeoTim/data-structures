81a260d243b5af9be942e61a72a99e11001eae30
hisimagination <joel.design@icloud.com>
2014-06-04 19:24:54 -0700

    Completed funcional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size ++;
   };
 
   someInstance.dequeue = function(){
+    var result;
+    if(size > 0){
+      size --;
+      result = storage[size];
+      delete storage[size];
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
